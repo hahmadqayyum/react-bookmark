8ac97cb073229dc3fb09552df40c74982ba83022
hahmadqayyum <65291999+hahmadqayyum@users.noreply.github.com>
2021-02-23 12:41:50 -0800

    Update ApolloClient.js

--- src/ApolloClient.js
+++ src/ApolloClient.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: "/.netlify/functions/bookmarks",
+    uri: "https://bookmark-ahmad.netlify.app/.netlify/functions/graphql",
     fetch,
   }),
   cache: new InMemoryCache(),
